f17492b2f80d7b68d62481f64ef61e43480b9273
Constantine Fedenko <cfedenko@gmail.com>
2013-05-30 12:57:37 +0300

    ticket:1 initial migration

--- test42/settings.py
+++ test42/settings.py
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'test42.apps.homepage',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
