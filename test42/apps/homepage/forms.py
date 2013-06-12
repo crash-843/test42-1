15223445e42ea454e8d24bd79d5ddfe9bd3b5904
Constantine Fedenko <cfedenko@gmail.com>
2013-06-12 16:38:48 +0300

    ticket:6 ajax image field

--- test42/apps/homepage/forms.py
+++ test42/apps/homepage/forms.py
@@ -1,14 +1,18 @@
 from django import forms
+from ajax_upload.widgets import AjaxClearableFileInput
 from .models import Info
+
 
 class DatePickerInput(forms.widgets.DateInput):
     def render(self, name, value, attrs=None):
         attrs.update({"class": "date-picker"})
         return super(DatePickerInput, self).render(name, value, attrs)
 
-class InfoForm(forms.ModelForm):
-    birthday = forms.DateField(widget=DatePickerInput())
-    photo = forms.FileField(widget=forms.FileInput())
 
+class InfoForm(forms.ModelForm):
     class Meta:
         model = Info
+        widgets = {
+            'birthday': DatePickerInput,
+            'photo': AjaxClearableFileInput
+        }
