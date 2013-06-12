78503b5d95eb192c88b5bc6bca408566040468a7
Constantine Fedenko <cfedenko@gmail.com>
2013-06-12 10:23:28 +0300

    ticket:6 added datepicker

--- test42/apps/homepage/forms.py
+++ test42/apps/homepage/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from .models import Info
 
+class DatePickerInput(forms.widgets.DateInput):
+    def render(self, name, value, attrs=None):
+        attrs.update({"class": "date-picker"})
+        return super(DatePickerInput, self).render(name, value, attrs)
 
 class InfoForm(forms.ModelForm):
+    birthday = forms.DateField(widget=DatePickerInput())
     photo = forms.FileField(widget=forms.FileInput())
 
     class Meta:
