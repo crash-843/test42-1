5cdd622d21518e9e16fc67b9d16fe574993c5c34
Constantine Fedenko <cfedenko@gmail.com>
2013-06-14 14:21:10 +0300

    ticket:6 js assets

--- test42/apps/homepage/forms.py
+++ test42/apps/homepage/forms.py
@@ -1,16 +1,39 @@
 from django import forms
+from django.utils.safestring import mark_safe
+
 from .models import Info
 
 
 class DatePickerInput(forms.widgets.DateInput):
     def render(self, name, value, attrs=None):
-        attrs.update({"class": "date-picker"})
-        return super(DatePickerInput, self).render(name, value, attrs)
+        rendered = super(DatePickerInput, self).render(name, value, attrs)
+        return rendered + mark_safe(u'''<script type="text/javascript">
+                $(function() {
+                    $('#id_%s').pikaday({
+                        format: "YYYY-MM-DD"
+                    });
+                });
+            </script>''' % name)
+
+    class Media:
+        css = {
+            'all': ("homepage/css/pikaday.css",)
+        }
+        js = ("homepage/js/lib/moment.min.js",
+              "homepage/js/lib/pikaday.js",
+              "homepage/js/lib/pikaday.jquery.js",)
 
 
 class InfoForm(forms.ModelForm):
-    birthday = forms.DateField(widget=DatePickerInput())
-    photo = forms.FileField(widget=forms.FileInput())
 
     class Meta:
         model = Info
+        widgets = {
+            'birthday': DatePickerInput,
+            'photo': forms.FileInput
+        }
+
+    class Media:
+        js = ("homepage/js/lib/jquery.form.min.js",
+              "homepage/js/spin.js",
+              "homepage/js/edit.js",)
